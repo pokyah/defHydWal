97744fb0db50145727be43c048df2ca8ed33b420
pokyah <thomas.goossens@protonmail.com>
2018-07-31 08:25:47 +0000

    colors the same on Ernage

--- R/dhw_plot.R
+++ R/dhw_plot.R
@@ -5,10 +5,15 @@
 #' @return ggplot graph
 #' @export
 dhw_plot <- function(data, int){
+  if (length(unique(data$Année)) == 2) {
+    colors = c("#4daf4a", "#e41a1c")
+  } else {
+    colors = c("#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#e41a1c")
+  }
   plot = ggplot(data, aes(x = Décade, y = Déficit)) +
     geom_line(aes(color = Année), na.rm = TRUE) +
     geom_point(aes(color = Année, shape = Année), na.rm = TRUE) +
-    scale_color_manual(values = c("#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#e41a1c")) +
+    scale_color_manual(values = colors) +
     scale_x_continuous(breaks = round(seq(0, 40, by = 4)), limits = c(0,40)) +
     scale_y_continuous(breaks = round(seq(-160,0, by = 20)), limits = c(-160,0)) +
     ylab("Déficit hydrique (mm)") +
