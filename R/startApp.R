80770a1171079afeea5950d9209971511b925612
pokyah <thomas.goossens@protonmail.com>
2018-08-01 08:25:10 +0000

    setting default port to 4326

--- R/startApp.R
+++ R/startApp.R
@@ -3,7 +3,7 @@
 #' @title Run the auto-reporting shiny app
 #' @return runs the shinyApp
 #' @export
-startApp <- function(port) {
+startApp <- function(port=4326) {
   appDir <- system.file("shiny", "reporting", package = "defHydWal")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `defHydWal`.", call. = FALSE)
